646f617ce27686ea545febf005da712be6a69aec
xsbfh <z6MkpzZDoFM7tPZPXqvaVCtMFxKJQAxghVRJZxsf8s8SqBvm@dev.smashchats.com>
2024-11-07 19:27:46 +0100

    refactor
    Signed-off-by: xsbfh <z6MkpzZDoFM7tPZPXqvaVCtMFxKJQAxghVRJZxsf8s8SqBvm@dev.smashchats.com>

--- tests/user.utils.ts
+++ tests/user.utils.ts
@@ -1,10 +1,10 @@
 import { Identity } from '2key-ratchet';
-import { SMEConfigJSONWithoutDefaults } from 'smash-node-lib';
+import { SMEConfigJSONWithoutDefaults, SmashMessaging } from 'smash-node-lib';
 
 export async function peerArgs(
     socketServerUrl?: string,
 ): Promise<[Identity, SMEConfigJSONWithoutDefaults[]]> {
-    const identity = await Identity.create(0, 1, 0, false);
+    const identity = await SmashMessaging.generateIdentity();
     const config = socketServerUrl
         ? [
               {
