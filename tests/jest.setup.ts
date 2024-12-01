91e4ff75b45873e4237a66c3c3969e3a31bb7082
xsbfh <z6MkpzZDoFM7tPZPXqvaVCtMFxKJQAxghVRJZxsf8s8SqBvm@dev.smashchats.com>
2024-12-01 00:04:18 +0000

    lint jest setup with process handling
    Signed-off-by: xsbfh <z6MkpzZDoFM7tPZPXqvaVCtMFxKJQAxghVRJZxsf8s8SqBvm@dev.smashchats.com>

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,18 +1,24 @@
-import { TIMEOUT_MS } from '@tests/time.utils.js';
 import { Logger, SmashMessaging } from 'smash-node-lib';
 
-const logger = new Logger('JEST');
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { TIMEOUT_MS } from './time.utils';
 
+const logger = new Logger('jest', 'INFO');
 jest.setTimeout(TIMEOUT_MS);
 
-beforeAll(() =>
-    (process as any).original.removeAllListeners('unhandledRejection'),
-);
-beforeEach(() =>
-    (process as any).original.on(
+type Process = NodeJS.Process & { actual: NodeJS.Process };
+
+beforeAll(() => {
+    console.log('>>> removing unhandledRejection listeners <<<');
+    (process as Process).actual.removeAllListeners('unhandledRejection');
+});
+
+beforeEach(() => {
+    (process as Process).actual.on(
         'unhandledRejection',
-        (reason: any, promise: Promise<any>) => {
+        (reason: unknown, promise: Promise<unknown>) => {
             SmashMessaging.handleError(reason, promise, logger);
         },
-    ),
-);
+    );
+});
