569d37525f38b35237d1d4744a674978fc2af80c
xsbfh <z6MkpzZDoFM7tPZPXqvaVCtMFxKJQAxghVRJZxsf8s8SqBvm@dev.smashchats.com>
2024-12-06 16:41:59 +0000

    graceful teardown
    adding grace period for async ops to finish after tests

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -2,7 +2,7 @@ import { Logger, SmashMessaging } from 'smash-node-lib';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { TIMEOUT_MS } from './time.utils';
+import { TIMEOUT_MS, delay } from './time.utils';
 
 const logger = new Logger('jest', 'INFO');
 jest.setTimeout(TIMEOUT_MS);
@@ -11,15 +11,20 @@ jest.setTimeout(TIMEOUT_MS);
 type Process = NodeJS.Process & { actual: NodeJS.Process };
 
 beforeAll(() => {
-    console.log('>>> removing unhandledRejection listeners <<<');
+    console.debug('>>> removing unhandledRejection listeners <<<');
     (process as Process).actual.removeAllListeners('unhandledRejection');
 });
 
 beforeEach(() => {
+    console.debug('>>> adding unhandledRejection listener <<<');
     (process as Process).actual.on(
         'unhandledRejection',
         (reason: unknown, promise: Promise<unknown>) => {
             SmashMessaging.handleError(reason, promise, logger);
         },
     );
+});
+
+afterAll(async () => {
+    await delay(1500);
 });
--- tests/jest.teardown.cjs
+++ tests/jest.teardown.cjs
@@ -1,4 +1,5 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 module.exports = function () {
     globalThis.__socketServer.close();
+    console.debug('>>> closing socket server <<<');
 };
