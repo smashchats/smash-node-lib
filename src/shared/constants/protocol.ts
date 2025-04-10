e8cc8d9c77a6fe967bd2c495c7d1874b9d8123bb
xsbfh <z6MkpzZDoFM7tPZPXqvaVCtMFxKJQAxghVRJZxsf8s8SqBvm@dev.smashchats.com>
2025-04-10 16:09:41 +0000

    add notes on max message size
    Signed-off-by: xsbfh <z6MkpzZDoFM7tPZPXqvaVCtMFxKJQAxghVRJZxsf8s8SqBvm@dev.smashchats.com>

--- src/shared/constants/protocol.ts
+++ src/shared/constants/protocol.ts
@@ -1,3 +1,6 @@
 export const EXPIRATION_TIME_MS = 7 * 24 * 60 * 60 * 1000; // 7 days
 export const SESSION_TTL_MS = EXPIRATION_TIME_MS;
-export const MAX_MESSAGE_SIZE = 512 * 1024; // 512KB
+// MAX_MESSAGE_SIZE set to 512KB to match socket.io cap
+// https://socket.io/fr/how-to/upload-a-file
+// Note that files shouldnt be sent as Smash messages, but with CDN protocols
+export const MAX_MESSAGE_SIZE = 512 * 1024;
